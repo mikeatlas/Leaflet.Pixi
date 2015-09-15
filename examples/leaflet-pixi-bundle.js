660465b943cd0977dfae3fadbc5e8fe17948f3d9
mikeatlas <mikeatlas@gmail.com>
2015-09-15 00:28:20 -0400

    I can make pix render 20k circles
    and sort of update them incorrectly on map move events.
    
    kinda yay.

--- examples/leaflet-pixi-bundle.js
+++ examples/leaflet-pixi-bundle.js
@@ -24704,8 +24704,9 @@ if (!global.cancelAnimationFrame) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],123:[function(require,module,exports){
+PIXI = require('pixi.js');
+	
 L.TileLayer.PixiTileLayer = L.TileLayer.extend({
-
  	options: {
  		option1: 1,
  		option2: 2,
@@ -24717,36 +24718,60 @@ L.TileLayer.PixiTileLayer = L.TileLayer.extend({
 
  	onAdd: function (map) {
 		this.map = map;
-		this.initPixiTileLayer()
+		this.initPixiTileLayer();
+		this.initMapEventHandlers();
 	},
 
  	initPixiTileLayer: function() {
  		var params = {};
- 		var mapOverlayPaneDiv = this.map.getPanes().overlayPane
- 		var PIXI = require('pixi.js');
+ 		var tilePaneDiv = this.map.getPanes().tilePane
+ 		var overlayPaneDiv = this.map.getPanes().overlayPane
 		var stage = new PIXI.Container();
+		this.stage = stage;
+		var mapContainerDiv = this.map.getContainer();
 		var renderer = PIXI.autoDetectRenderer(
-			mapOverlayPaneDiv.offsetWidth, 
-			mapOverlayPaneDiv.offsetHeight, 
-			{transparent: false}
+			mapContainerDiv.offsetWidth, 
+			tilePaneDiv.offsetHeight, 
+			{transparent: true}
 		);
-		mapOverlayPaneDiv.parentNode.lastChild.appendChild(renderer.view);
+		this.renderer = renderer;
+		overlayPaneDiv.parentNode.lastChild.appendChild(renderer.view);
 		renderer.view.style.position = "absolute";
 		renderer.view.style.top = "0px";
-		renderer.view.style.left = "0px";
 		renderer.render(stage);
 	},
 
+	initMapEventHandlers: function() {
+		this.map.on('move', this._mapMoved, this);
+	},
+
+	_mapMoved: function(moveEvent){
+		var pixelCenter = this.map.latLngToContainerPoint(moveEvent.target.getCenter());
+
+		var divTopLeft = this.map.getPixelOrigin();
+	
+        var x = divTopLeft.x;
+        var y = divTopLeft.y;
+
+        this.graphics.position.x = x;
+        this.graphics.position.y = y;
+        this.renderer.render(this.stage);
+	},
+
 	runTest: function(){
+		// test
 		var graphics = new PIXI.Graphics();
+		this.graphics = graphics
 		var porterSq = new L.LatLng(42.388349, -71.119355);
 		var referencePixel = this.map.latLngToContainerPoint(porterSq);
 
-		graphics.beginFill(0xFFFFFF);
+		this.graphics.beginFill(0xFFFFFF);
 		for(var i=0; i < 20000; i++) {
-		    graphics.drawRect(referencePixel.x + Math.random()* 1200.0, referencePixel.y + Math.random()*900.0, 2, 2);
+		    graphics.drawCircle(referencePixel.x + Math.random()* 1200.0, referencePixel.y + Math.random()*900.0, 2, 2);
 		}
-		graphics.endFill();
+		this.graphics.endFill();		
+		this.stage.addChild(this.graphics);
+		this.renderer.render(this.stage);
 	}
 
 });
